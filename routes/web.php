a105c823a303ef3ef410df58de605b2219e769a3
kohki-nakamura <nakamura@v-force.co.jp>
2019-04-15 03:05:20 +0900

    リスト機能のルーティングとコントローラの設定

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,21 @@
 <?php
+//リスト一覧画面
+Route::get('/','ListingsController@index');
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//リスト新規画面
+Route::get('/new', 'ListingsController@new')->name('new');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//リスト新規処理
+Route::post('/listings', 'ListingsController@store');
+
+//リスト更新画面
+Route::get('/listingsedit/{listing_id}', 'ListingsController@edit');
+
+//リスト更新処理
+Route::post('/listing/edit', 'ListingsController@update');
+
+//リスト削除処理
+Route::get('/listingsdelete/{listing_id}', 'ListingsController@destroy');
 
 Auth::routes();
 
