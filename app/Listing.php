af1eb10f654d49b359d942be95aeb3943e9c484f
kohki-nakamura <nakamura@v-force.co.jp>
2019-04-17 23:39:49 +0900

    カード作成機能の実装

--- app/Listing.php
+++ app/Listing.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     // hasMany設定
-    public function cards(){
-        return $this->hasMany('App/Card');
+    public function cards()
+    {
+        return $this->hasMany('App\Card');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,21 @@ Route::get('/listingsdelete/{listing_id}', 'ListingsController@destroy');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//カード新規画面
+Route::get('listing/{listing_id}/card/new', 'CardsController@new')->name('new_card');
+
+//カード新規処理
+Route::post('/listing/{listing_id}/card', 'CardsController@store');
+
+//カード詳細画面
+Route::get('listing/{listing_id}/card/{card_id}', 'CardsController@show');
+
+//カード編集画面
+Route::get('listing/{listing_id}/card/{card_id}/edit', 'CardsController@edit');
+
+//カード更新処理
+Route::post('/card/edit', 'CardsController@update');
+
+//カード削除処理
+Route::get('listing/{listing_id}/card/{card_id}/delete', 'CardsController@destroy');
