3c348186c0751cc6347c3ef2f4583dfc955632fd
kohki-nakamura <nakamura@v-force.co.jp>
2019-04-17 21:48:36 +0900

    CardとListingの紐付け

--- app/Listing.php
+++ app/Listing.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Listing extends Model
 {
-    //
+    // hasMany設定
+    public function cards(){
+        return $this->hasMany('App/Card');
+    }
 }
